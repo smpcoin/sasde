f10f2f510f1471e7e857b140042dd988e55bba31
fileweb3serve <96048793+fileweb3serve@users.noreply.github.com>
2026-03-17 22:23:32 +0800

    '

---
+++ assets/cursor-85Z23K4e.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-BrvJ686W.js";import"./index-5x2wE9E5.js";import"./index.es-BZx9vXv9.js";import"./events-DQ172AOg.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
